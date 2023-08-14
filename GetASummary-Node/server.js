7c3b552f22606d38c88d7ecfbf7f78fad7636aac
Biz Dev <43341818+bizdev324@users.noreply.github.com>
2023-08-14 08:07:43 -0700

    initial update

---
+++ GetASummary-Node/server.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const pdfParse = require('pdf-parse');
+const fs = require('fs');
+const cors = require('cors');
+const { OpenAIApi } = require('openai');
+const openai = new OpenAIApi({ api_key: process.env.OPENAI_API_KEY });
+const app = express();
+app.use(cors());
+
+// Define the path to the build directory
+const buildPath = path.join(__dirname, '..', 'GetASummary-React', 'get-a-summary', 'build');
+
+// Multer setup for file uploads
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/convert-pdf', upload.single('pdfFile'), async (req, res) => {
+    try {
+        console.log(`Received file ${req.file.originalname}`);  // Log when the file is received
+        const dataBuffer = fs.readFileSync(req.file.path);
+        const data = await pdfParse(dataBuffer);
+        console.log(`Converted file ${req.file.originalname} to text`);  // Log when the file is converted to text
+        res.json({ text: data.text });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to convert PDF to text.' });
+    } finally {
+        // Clean up uploaded file
+        fs.unlink(req.file.path, (err) => {
+            if (err) console.error(`Failed to delete file ${req.file.path}`);
+        });
+    }
+});
+
+const axios = require('axios');
+
+app.post('/get-answer', express.json(), async (req, res) => {
+    const { pdfText, userQuestion } = req.body;
+    const messages = [
+        {
+            "role": "system",
+            "content": pdfText
+        },
+        {
+            "role": "user",
+            "content": userQuestion
+        }
+    ];
+
+    try {
+        const gptResponse = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model: 'gpt-3.5-turbo-16k-0613',
+            messages,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const answer = gptResponse.data.choices[0].message.content.trim();
+        res.json({ answer });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to get answer.' });
+    }
+});
+
+
+
+
+
+// Serve static files from the React build directory
+console.log(`Serving static files from ${buildPath}`);
+app.use(express.static(buildPath));
+
+// Use a wildcard route as a fallback for any other requests
+app.get('*', (req, res) => {
+    console.log(`Serving index.html from ${buildPath}`);
+    res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
